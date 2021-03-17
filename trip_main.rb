e45a3e39b88c76e55df2ee84de3d044b8ef3d565
Megurin <tutimur_0521@yahoo.co.jp>
2021-03-17 20:06:47 +0900

    旅行プログラム（メソッド）の作成

---
+++ trip_main.rb
@@ -0,0 +1,13 @@
+require "./try_trip_method.rb"
+
+# 旅行プラン
+plans = [
+  { place: "沖縄", price: 10000 },
+  { place: "北海道", price: 20000 },
+  { place: "九州", price: 15000 }
+]
+
+disp_plans(plans) #プランの表示
+chosen_plan = choose_plan(plans)　#プランの番号を選択
+person_num = enter_number(chosen_plan) #予約人数を入力
+calculate_price(chosen_plan, person_num) #合計料金を表示
---
+++ try_trip_method.rb
@@ -0,0 +1,45 @@
+
+ # プランの表示
+def disp_plans(plans)
+  puts "旅行プランを選択してください。"
+  plans.each.with_index(1) do |plan,i|
+    puts "#{i}. #{plan[:place]}旅行 (#{plan[:price]}円)"
+  end
+end
+
+# プランの番号を選択
+def choose_plan(plans)
+  while true
+    print "プランの番号を選択>"
+    select_plan_num = gets.to_i
+    break if (1..3).include?(select_plan_num)
+    puts "1~3の番号を入力してください。"
+  end
+  plans[select_plan_num - 1]
+end
+
+
+# 予約人数を入力
+def enter_number(chosen_plan)
+  puts <<~TEXT
+  #{chosen_plan[:place]}旅行ですね。
+  何名で予約されますか？
+  TEXT
+  while true
+    print "人数を入力 > "
+    person_num = gets.to_i
+    break if person_num >= 1
+    puts "1以上を入力してください。"
+  end
+  person_num
+end
+
+# 合計料金を表示
+def calculate_price(chosen_plan, person_num)
+  total_price = chosen_plan[:price]*person_num
+  if person_num >= 5
+    puts "5名以上ですので10%割引となります。"
+    total_price *= 0.9
+  end
+  puts "合計料金は#{total_price.floor}円になります。"
+end
